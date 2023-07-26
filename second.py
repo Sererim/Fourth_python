bd5493f8654fd6ae65c0fe9ddbe00ade9facf7c8
Sererim <danielleymanw@gmail.com>
2023-07-26 19:06:11 +0300

    Final commit.

--- second.py
+++ second.py
@@ -1,3 +1,20 @@
 # Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь, где ключ — значение переданного аргумента, а значение — имя аргумента.
 # Если ключ не хешируем используйте его строковое представление.
+import inspect
 
+
+def pack(key) -> dict:
+    call = inspect.currentframe().f_back.f_locals.items()
+    try:
+        return {key.__hash__(): name for name, value in call if value is key}
+    except TypeError:
+        return {key.__repr__(): name for name, value in call if value is key}
+
+
+if __name__ == "__main__":
+    h = 0
+    print(pack(h))
+    l = (0, 0)
+    print(pack(l))
+    s = set(l)
+    print(pack(s))
